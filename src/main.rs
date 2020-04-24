fcd08cd9a088be0d00efd3126c74f158ff5f8031
trygve@stasjoner <trygve.woldseth@gmail.com>
2020-04-24 13:17:11 +0200

    v2

--- src/main.rs
+++ src/main.rs
@@ -1,158 +1,69 @@
+mod sim_grid;
+mod helpers;
+mod person;
+
+
+use crate::helpers::*;
+use crate::person::*;
+
+use sim_grid::SimGrid;
 use rand::Rng;
-///
-/// Lag førs en simulator som virke i en fil, kan i ettertid
-/// splitt den opp i flere moduler
 
+struct Simulator<'a> {
+    persons: Vec<Person>,
+    sim_grid: SimGrid<'a>,
+    simulator_ticks: i32,
 
-/// Persjon og funcs
-
-
-#[derive(Clone, Copy)]
-enum InfectedState{
-    Susceptible,
-    Infectious,
-    Recovered,
-    Dead,
-}
-
-impl InfectedState{
-    fn to_sus(self) -> Self {
-        match self {
-            InfectedState::Susceptible => InfectedState::Recovered,
-            v => v
-        }
-    }
-}
-
-
-pub struct Position {
-    pub x: usize,
-    pub y: usize,
-}
-
-
-
-struct Person {
-    age: u32,
-    state: InfectedState,
-    pos: Position,
-    infected_tick: i32,
-}
-
-
-impl Person {
-    // pub 
-    pub fn act(&mut self, rng_val: i32, sim_grid: &mut SimGrid){
-        let has_moved = match rng_val{
-            1 => sim_grid.try_move(&mut self.pos, Direction::Up),
-            2 => sim_grid.try_move(&mut self.pos, Direction::Up),
-            3 => sim_grid.try_move(&mut self.pos, Direction::Up),
-            4 => sim_grid.try_move(&mut self.pos, Direction::Up),
-            _ => false
-        };
-    }
-
-    // priv
-
-    
-}
-/// grid og funcs
-
-
-
-struct SimGrid<'a> {
     x_size: usize,
     y_size: usize,
-    grid: Vec<Option<&'a mut InfectedState>>,
+
+    start_pop_size: i32,
+    start_infected_size: i32,
 
 }
 
-enum Direction{
-    Up,
-    Right,
-    Left,
-    Down,
-}
-///
-/// 0 marks a free spot 
-/// 1 is Susceptible
-/// 2 is Infectious
-/// 3 is Recovered
-/// 4 is dead
-impl<'a> SimGrid<'a> {
-    fn new(x_size: usize, y_size: usize) -> SimGrid<'a> {
-        let mut a : Vec<Option<&'a mut InfectedState>> = Vec::new();
-        for _ in 0..(x_size*y_size) {
-            a.push(Option::<& mut InfectedState>::None)
-        }
-        SimGrid {
+impl<'a> Simulator<'a> {
+    fn new(x_size: usize, y_size: usize, start_pop_size: i32, start_infected_size: i32) -> Simulator<'a>{
+        let sg = SimGrid::new(x_size, y_size);
+        Simulator{
+            persons: Vec::new(),
+            sim_grid: sg,
+            simulator_ticks: 0,
+
             x_size,
             y_size,
-            grid: a,
+
+            start_pop_size,
+            start_infected_size,
         }
     }
-    // pub
-    pub fn try_move(&mut self, pos: &mut Position, dir: Direction) -> bool{
-        let maybe_new_pos = match dir {
-            Direction::Up    => Position{x:pos.x, y: pos.y + 1},
-            Direction::Down  => Position{x:pos.x, y: pos.y - 1},
-            Direction::Right => Position{x:pos.x + 1, y: pos.y},
-            Direction::Left  => Position{x:pos.x - 1, y: pos.y},
-        };
+
+    fn populate(&'a mut self){
+        let mut rng = rand::thread_rng();
+        let mut num_infected = 0;
         
-        if self.is_free(&maybe_new_pos) {
-            let mover_val = self.get_value_at(&pos);
-            // match mover_val {
-            //     None => (),
-            //     Some(mut InfectedState) => InfectedState
-            // }
-            if let Some(ref InfectedState) = mover_val {
-                InfectedState.to_sus();
-            }
-            self.set_value_at(&maybe_new_pos, mover_val);
-            self.set_value_at(&pos, None);
-            pos.x = maybe_new_pos.x;
-            pos.y = maybe_new_pos.y;
-            return true
-        } else {
-            return false
+
+        for n in 0..self.start_pop_size {
+            let infected_state = if num_infected < self.start_infected_size {
+                num_infected += 1;
+                InfectedState::Infectious
+            } else {
+                InfectedState::Susceptible
+            };
+            let pos = Position{x: rng.gen_range(0, self.x_size), y: rng.gen_range(0, self.y_size)};
+            let p = Person::new(infected_state, pos);
+            self.persons.push(p);
         }
-    }
 
-    pub fn update_sir_state(&self){
-
-    }
-
-    // priv
-    fn set_value_at(&mut self, pos: &Position, val: Option<&'a mut InfectedState>){
-        let new_index = self.get_index(pos);
-        self.grid[new_index] = val;
-    }
-
-    fn get_value_at(&self, pos: &Position) -> Option<&'a mut InfectedState> {
-        self.grid[self.get_index(pos)]
-    }
-    fn is_valid(&self, pos: &Position) -> bool {
-        if pos.x >= self.x_size || pos.y >= self.y_size {
-            return false
-        } else {
-            return true
+        for pers in self.persons.iter(){
+            pers.init_move(&mut self.sim_grid)
+            
         }
+
     }
 
-    fn get_index(&self, pos: &Position) -> usize {
-        (pos.y * self.y_size) + pos.x
-       
-    }
 
-    fn is_free(&self, pos: &Position) ->  bool{
-        if !self.is_valid(pos) {return false}
-        let val = self.get_value_at(pos);
-        return match val {
-            None => true,
-            _    => false,
-        }
-    }
+
 }
 
 fn main() {
